eecefef9a988277bd4b6021ed0b1e9cf971d07de
Safvan <safvanpmcz@gmail.com>
2022-07-28 12:45:52 +0530

    Solution for Question 2 added

---
+++ Question2.java
@@ -0,0 +1,15 @@
+public class Question2
+{
+    public static void main(String[] args)
+    {
+        int n=4;
+        for (int row=1;row<=n;row++)
+        {
+            for (int colm=1;colm<=n;colm++)
+            {
+                System.out.print(row);
+            }
+            System.out.println();
+        }
+    }
+}
